ba41559fe85789286131b9ebf2416c714a5e85d8
GuddintiPriyanka <priyaguddinti57@gmail.com>
2020-05-20 19:39:12 +0530

    frst cmmt

---
+++ sam1/src/main/java/sam/sam1/App.java
@@ -0,0 +1,13 @@
+package sam.sam1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
